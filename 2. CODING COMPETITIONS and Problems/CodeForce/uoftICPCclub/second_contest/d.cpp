19303c8d62a747b22be6648c7650fda4db869556
Xin Lei Lin <xinlei55555@gmail.com>
2023-09-30 12:54:20 -0400

    new contests

--- 2. CODING COMPETITIONS and Problems/CodeForce/uoftICPCclub/second_contest/d.cpp
+++ 2. CODING COMPETITIONS and Problems/CodeForce/uoftICPCclub/second_contest/d.cpp
@@ -8,10 +8,11 @@ int main(){
     while(t--){
         ll n; cin>>n;
         ll arr[n]; cin>>arr[0] >>arr[1];
-        ll ans=1, prev=arr[1], prev_num=1, next_num=1;
+        // ll ans=1, prev=arr[1], prev_num=1, next_num=1;
 
         //!! AA THIS IS WHAT I DID WRONG, I should have kept the number of elements in each layer, since the chunks of sorted can occur under a every element of the layer!
-        vector<int> layers = {1}
+        // vector<int> layers = {1}
+        // vector<int> layers   
         for(ll i=2;i<n;i++){
             cin>>arr[i];
             //the previous is the lowest of the level
