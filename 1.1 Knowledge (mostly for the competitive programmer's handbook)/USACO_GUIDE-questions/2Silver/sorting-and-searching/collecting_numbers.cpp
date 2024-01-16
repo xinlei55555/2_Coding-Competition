727ad7d25317973706da2d53bd59dce80df86827
Xin Lei Lin <xinlei55555@gmail.com>
2024-01-16 10:42:26 -0500

    new problem

---
+++ 1.1 Knowledge (mostly for the competitive programmer's handbook)/USACO_GUIDE-questions/2Silver/sorting-and-searching/collecting_numbers.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define debug(x) cerr<<#x<<" "<<x<<"\n";
+
+
+// int main(){
+//     int n; cin>>n; 
+//     vector<int> arr;
+//     int mx = 0;
+//     //find the longest decreasing sequene.
+//     //use a difference array, and find the largest number of decreasing ones?
+
+//     //use two pointers.
+
+//     for(int i=0;i<n;i++){
+//         if()
+//     }
+
+//     return 0;
+// }
+//!
+/**!
+I’ll share my approach, so the main observation is the following :
+If the number $x$  
+occurs before $x+1$  
+then you can always take both of them in a single round 
+and hence it won’t contribute anything to the answer but if $x$ comes after $x+1$ 
+then we cannot take them  in the single round hence we add $1$ to the final answer.*/
+int main(){
+    int n,ans=1 ;
+    cin >> n ;
+    vector<int>a(n),b(n);
+    for(int &x:a)
+        cin >> x,x--  ;
+    for(int i=0;i<n;i++)
+        b[a[i]]=i ;
+    for(int i=1;i<n;i++)
+        ans+=b[i]<b[i-1];
+    cout << ans  ;
+}
--- 1.1 Knowledge (mostly for the competitive programmer's handbook)/USACO_GUIDE-questions/2Silver/sorting-and-searching/more_operations_on_sorted_sets/concert_ticket.cpp
+++ 1.1 Knowledge (mostly for the competitive programmer's handbook)/USACO_GUIDE-questions/2Silver/sorting-and-searching/more_operations_on_sorted_sets/concert_ticket.cpp
@@ -1,15 +1,90 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// typedef long long ll;
+// #define debug(x) cerr<<#x<< " "<<x<<"\n";
+
+// bool cmp(pair<ll, ll> a, pair<ll, ll>  b ){
+//     return a.first<b.first;
+// }
+
+// int main(){
+//     ll a,m, n; cin>> n>>m;
+//     vector<pair<ll, ll>> tickets(n), people(m); 
+//     for(int i=0;i<n;i++){
+//         cin>>a;tickets[i] = {a, i};
+//     }
+//     for(int i=0;i<m;i++){
+//         cin>>a; people[i] = {a, i};
+//     }
+//     sort(tickets.begin(), tickets.end(), cmp);
+//     sort(people.begin(), people.end(), cmp);
+//     vector<pair<ll, ll>> answer;
+//     // for(auto x: tickets) debug(x.first)
+//     // for(auto x: people) debug(x. first)
+//     ll j = n-1, i = m-1;
+//     while(i>=0 and j>=0){
+//         while(j>=0){
+//             if(people[i].first>=tickets[j].first){
+//                 // debug(people[i].first)
+//                 // debug(tickets[j].first)
+//                 answer.push_back({people[i].second, tickets[j].first});
+//                 j--;
+//                 break;
+//             }else j--;
+//         }
+//         i--;
+//     }
+//     //then loop through the rest and mak i
+//     sort(answer.begin(), answer.end(), cmp);
+//     // for(auto x: answer) {debug(x.second)
+//     // debug(x.first)}
+
+// //somehow this part doesn't run, but if it did... it would work. It runs when I call debug()
+//     i=0, j=0;
+//     while(i<m){
+//         // cout<<"";
+//         // debug("HI")
+//         if(answer[j].first == i){
+//             cout<<answer[j].second<<"\n";
+//             j=answer[j+1].first; 
+//         }
+//         else cout<<-1<<"\n";
+//         i++;
+//     }
+//     return 0;
+// }
+
+//correct idea above ^, except, if you wish to find the closest element smaller than a certain element, use lower_bound/upper_bounds in the sets!
 #include <bits/stdc++.h>
+
 using namespace std;
 
-typedef long long ll;
-#define debug(x) cerr<<#x<< " "<<x<<"\n";
+// variables used for the current problem
+int n, m, h, t;
+multiset<int> tickets;
 
-int main(){
-    ll m, n; cin>> n>>m;
-    
-    for(int i=0;i<n;i++){
+void solve() {
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) {
+		cin >> h;
+		tickets.insert(h);
+	}
+	for (int i = 0; i < m; ++i) {
+		cin >> t;
+		auto it = tickets.upper_bound(t);
+		if (it == tickets.begin()) {
+			cout << -1 << "\n";
+		} else {    
+			cout << *(--it) << "\n";
+			tickets.erase(it);
+		}
+	}
+}
 
-    }
-
-    return 0;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	solve();
+	return 0;
 }
